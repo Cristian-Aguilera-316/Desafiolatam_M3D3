46e53a3abcec2b946aaa11555512dc9a126c01ce
Cristian-Aguilera-316 <cristian.ap316@gmail.com>
2024-04-12 12:21:54 -0400

    creacion del proyecto

---
+++ req4/4.1-4.2/assets/js/script.js
@@ -0,0 +1,23 @@
+//  REQ-4.1 Crea 4 divs que tengan alto y ancho de 200px y de colores azul, rojo, verde y
+//  amarillo (insertar estilos con atributo style). Cada uno de los divs debe tener
+//  un identificador único.
+
+//  REQ-4.2 Dentro del script agrega el evento para que al hacer click a cada uno de los
+//  divs, este cambie de color al color negro. Utiliza addEventListener para
+//  agregar el evento.
+
+/* R 4.1 - 4.2 */
+
+const azul = document.getElementById('azul');
+const rojo = document.getElementById('rojo');
+const verde = document.getElementById('verde');
+const amarillo = document.getElementById('amarillo');
+
+function cambiarColor(event) {
+    event.target.style.backgroundColor = 'black';
+}
+
+azul.addEventListener('click', cambiarColor);
+rojo.addEventListener('click', cambiarColor);
+verde.addEventListener('click', cambiarColor);
+amarillo.addEventListener('click', cambiarColor);
